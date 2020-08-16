43fac4d8d886ddc1a153620116e17d77d883d5a0
Yasuyamakyosuke <toyojindai@gmail.com>
2020-08-16 16:36:42 +0900

    error_solution

--- app/controllers/user/shops_controller.rb
+++ app/controllers/user/shops_controller.rb
@@ -6,26 +6,25 @@ class User::ShopsController < ApplicationController
 
 	def index
 		@shops = Shop.all
-	    @shops = @shops.where("shop_name like ?", "%#{params[:name]}%") if params[:name].present?
+	  @shops = @shops.where("shop_name like ?", "%#{params[:name]}%") if params[:name].present?
 		@shops = @shops.where(genre_id: params[:genre_id]) if params[:genre_id].present?
 		@shops = @shops.where(station_id: params[:station_id]) if params[:station_id].present?
-		if params[:sort_type] == 'shop_comment_count'
-            @shops = @shops.select('shops.*', 'count(shop_comments.shop_id) AS shop_comment_count')
-                    .left_joins(:shop_comments)
-                    .group('shops.id')
-                    #.group('shop_comments.shop_id')
-                    .order('count(shop_comments.shop_id) desc')
-		elsif params[:sort_type] == 'favorites_count'
-              @shops = @shops.select('shops.*', 'count(favorites.shop_id) AS favorites_count')
-                    .left_joins(:favorites)
-                    .group('favorites.shop_id')
-                    .order('count(favorites.shop_id) desc')
-        elsif params[:sort_type] == 'shop_name'
-              @shops = @shops.order('shop_name asc')
-        elsif params[:sort_type] == 'average_score'
-              @shops = @shops.order('average_score desc')
-        end
-        @shops= @shops.page(params[:page])
+		@shops = if params[:sort_type] == 'shop_comment_count'
+					      @shops.left_joins(:shop_comments)
+						          .group('shops.id')
+						          .order('count(shop_comments.shop_id) desc')
+				                    # .select('shops.*', 'count(shop_comments.shop_id) AS shop_comment_count')
+				                    #.group('shop_comments.shop_id')
+ 						 elsif params[:sort_type] == 'favorites_count'
+ 				        @shops.left_joins(:favorites)
+ 						 		      .group('favorites.shop_id')
+ 						 		      .order('count(favorites.shop_id) desc')
+ 				     elsif params[:sort_type] == 'shop_name'
+ 				       @shops.order('shop_name asc')
+ 				     elsif params[:sort_type] == 'average_score'
+ 				       @shops.order('average_score desc')
+ 				     end
+    @shops= @shops.page(params[:page])
 	end
 
 	def show
